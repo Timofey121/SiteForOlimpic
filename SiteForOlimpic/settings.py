5a1491e15af1432c57106b98967858e45c0c601b
“Timofey121” <ale3jurtaev@gmail.com>
2023-08-01 14:45:31 +0400

    gen

--- SiteForOlimpic/settings.py
+++ SiteForOlimpic/settings.py
@@ -18,7 +18,7 @@ from environs import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(os.path.join(BASE_DIR, '.env.dev'))
+load_dotenv(os.path.join(BASE_DIR, '.env.prod'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
